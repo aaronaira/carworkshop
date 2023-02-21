cdb35d3a716726008ca8e869581589bbdc401d49
zenlaboratory <zenlaboratory@gmail.com>
2023-02-21 03:56:18 +0100

    A lot of stuff

--- src/carworkshop/clases/Cita.java
+++ src/carworkshop/clases/Cita.java
@@ -2,32 +2,32 @@ package carworkshop.clases;
 
 public class Cita {
 
-        private int id_vehiculo;
-        private int id_cliente;
-        private int id_diagnostico;
+    private int id_vehiculo;
+    private int id_cliente;
+    private int id_diagnostico;
 
-        public int getId_vehiculo(){
-            return id_vehiculo;
-        }
-
-        public void setId_vehiculo(int id_vehiculo){
-            this.id_vehiculo=id_vehiculo;
-        }
-
-        public int getId_cliente(){
-            return id_cliente;
-        }
-
-        public void setId_cliente(int id_cliente){
-            this.id_cliente=id_cliente;
-        }
-
-        public int getId_diagnostico(){
-            return id_diagnostico;
-        }
-
-        public void setId_diagnostico(int id_diagnostico){
-            this.id_diagnostico=id_diagnostico;
-        }
+    public int getId_vehiculo() {
+        return id_vehiculo;
     }
+
+    public void setId_vehiculo(int id_vehiculo) {
+        this.id_vehiculo = id_vehiculo;
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public int getId_diagnostico() {
+        return id_diagnostico;
+    }
+
+    public void setId_diagnostico(int id_diagnostico) {
+        this.id_diagnostico = id_diagnostico;
+    }
+}
 
