f8b910bc90b47102627016780d9b5a531286f972
zenlaboratory <zenlaboratory@gmail.com>
2023-02-24 19:41:06 +0100

    Added ui packages login & dialogs.

---
+++ src/main/java/carworkshop/ui/login/login.java
@@ -0,0 +1,10 @@
+package carworkshop.ui.login;
+
+import javax.swing.*;
+
+public class login {
+    private JPanel panel1;
+    private JPasswordField passwordField1;
+    private JTextField textField1;
+    private JButton LOGINButton;
+}
